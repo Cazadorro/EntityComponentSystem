6857302e0013c12ec4e494d19ef9401f1f736f55
Shae Bolt <snb4y4@mst.edu>
2017-03-23 22:53:50 -0500

    systems register entities through loop

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@ int main() {
     std::vector<Component*> component_pool;
     std::cout << "Hello, World!" << std::endl;
     std::string input_string = "";
+
     Entity player;
     InputComponent input_component(&input_string);
     component_pool.push_back(&input_component);
@@ -23,11 +24,37 @@ int main() {
     NameComponent name("Entity_1");
     component_pool.push_back(&name);
     DisplayCharComponent dchar("@");
+    component_pool.push_back(&dchar);
     player.addComponent(&input_component);
     player.addComponent(&pos_component);
     player.addComponent(&vel_component);
     player.addComponent(&name);
     player.addComponent(&dchar);
+
+    Entity prop1;
+    PositionComponent posc1(3,3,0);
+    component_pool.push_back(&posc1);
+    NameComponent name1("Prop1");
+    component_pool.push_back(&name1);
+    DisplayCharComponent dchar1("H");
+    component_pool.push_back(&dchar1);
+
+    prop1.addComponent(&posc1);
+    prop1.addComponent(&name1);
+    prop1.addComponent(&dchar1);
+
+
+    Entity prop2;
+    PositionComponent posc2(8,10,3);
+    component_pool.push_back(&posc1);
+    NameComponent name2("Prop2");
+    component_pool.push_back(&name2);
+    DisplayCharComponent dchar2("5");
+    component_pool.push_back(&dchar2);
+
+    prop2.addComponent(&posc2);
+    prop2.addComponent(&name2);
+    prop2.addComponent(&dchar2);
 
     PhysicsSystem physics;
     InputSystem input;
@@ -37,9 +64,11 @@ int main() {
     systems.push_back(&physics);
     systems.push_back(&display);
 
-    physics.registerEntity(player);
-    input.registerEntity(player);
-    display.registerEntity(player);
+    for(auto system: systems){
+        system->registerEntity(player);
+        system->registerEntity(prop1);
+        system->registerEntity(prop2);
+    }
 
     while (input_string != "end"){
         std::getline(std::cin,input_string);
